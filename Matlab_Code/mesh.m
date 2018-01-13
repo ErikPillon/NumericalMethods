942d18805a2a0e7697decc61931b9a87cbc2df98
ErikPillon <erik.pillon@gmail.com>
2018-01-13 14:17:24 +0100

    Slightly modified the .m files, section 4.1 completed, sterted section 4.2

--- Matlab_Code/mesh.m
+++ Matlab_Code/mesh.m
@@ -1,13 +1,8 @@
-function [ E ] =mesh( lx,ly,nx,ny,M)
-
+function [E]=mesh(lx,ly,nx,ny,M)
 
 for e=1:nx*(ny-1)
     if M(e,1)!=lx
-        E(e,:)=(e,e+1,e+nx+1,e+nx);
-        
-       
- 
-    end
-    
+       E(e,:)=(e,e+1,e+nx+1,e+nx);
+end
 end
 
--- Matlab_Code/points.m
+++ Matlab_Code/points.m
@@ -1,16 +1,16 @@
 function [M] = points( lx,ly,nx,ny)
 
-x=0,y=0,i=0;
+x=0; %initialize all the variables to zero
+y=0;
+i=0;
 dy=ly/ny;
 dx=lx/nx;
+% use the loops for both x and y
 for y=0:dy:ly
     for x=0;dx;lx
-        M(i,:)= [x,y];
-      
-        i=i+1;
+        M(i,:)= [x,y]; %save the point
+        i=i+1; %update the counter
     end
-    
-  
 end
 end
 
