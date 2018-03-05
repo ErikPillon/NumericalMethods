8f0bf6555467e93168a1b53ed39717ff5d1f7ad3
Erik Pillon <erik.pillon@gmail.com>
2018-03-05 23:06:16 +0100

    Developed the third dl. Created Jacobian and alpha function

--- Matlab_Code/alpha.m
+++ Matlab_Code/alpha.m
@@ -1,21 +1,30 @@
-function [alfa, dalfa_u, dalfa_v] = alpha(u,v)
-% u, v: nodes in which the function must be evaluated
+function [alfa, dalfa_xi, dalfa_eta] = alpha(xi,eta)
+% -------- INPUT ------------
+% xi, eta: nodes in which the function must be evaluated
+% 
+% -------- OUTPUT -----------
+% alfa:      4x1 vector with the values of the four alfas evaluated at (xi,eta)
+% dalfa_xi:  4x1 vector of the partial derivatives of alfas wrt xi evaluated
+%            at (xi,eta)
+% dalfa_eta: 4x1 vector of the partial derivatives of alfas wrt eta evaluated
+%            at (xi,eta)
 
-alfa(1)=1/4*(1-u).*(1-v);
-alfa(2)=1/4*(1+u).*(1-v);
-alfa(3)=1/4*(1+u).*(1+v);
-alfa(4)=1/4*(1-u).*(1+v);
+% alfa functions 
+alfa(1)=1/4*(1-xi).*(1-eta);
+alfa(2)=1/4*(1+xi).*(1-eta);
+alfa(3)=1/4*(1+xi).*(1+eta);
+alfa(4)=1/4*(1-xi).*(1+eta);
 
-%evaluation of the derivative respect to u
-dalfa_u(1)=-1/4.*(1-v);
-dalfa_u(2)=1/4.*(1-v);
-dalfa_u(3)=1/4.*(1+v);
-dalfa_u(4)=-1/4.*(1+v);  
+% evaluation of the derivative respect to u
+dalfa_xi(1)=-1/4.*(1-eta);
+dalfa_xi(2)=1/4.*(1-eta);
+dalfa_xi(3)=1/4.*(1+eta);
+dalfa_xi(4)=-1/4.*(1+eta);  
 
-%evaluation of the derivative respect to v
-dalfa_v(1)=-1/4.*(1-u);
-dalfa_v(2)=-1/4.*(1+u);
-dalfa_v(3)=1/4.*(1+u);
-dalfa_v(4)=1/4.*(1-u);
+% evaluation of the derivative respect to v
+dalfa_eta(1)=-1/4.*(1-xi);
+dalfa_eta(2)=-1/4.*(1+xi);
+dalfa_eta(3)=1/4.*(1+xi);
+dalfa_eta(4)=1/4.*(1-xi);
 
 end
